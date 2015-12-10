15b79b2246814c3c50228b1463bb48b1cd5d2236
App Academy Student <student@appacademy.io>
2015-12-10 14:24:30 -0800

    phase 3

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -3,12 +3,17 @@ var ReactDOM = require('react-dom');
 var ApiUtil = require('./util/apiUtil');
 var PokemonStore = require('./stores/pokemon.js');
 var PokemonsIndex = require('./components/pokemons/pokemonsIndex');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var App = require('./components/app.jsx');
 
-// for testing
-// window.PokemonStore = require('./stores/pokemon.js');
-// window.ApiUtil = require('./util/apiUtil');
+var routes = (
+  <Route path="/" component={App}></Route>
+);
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  ReactDOM.render(<PokemonsIndex />, document.getElementById('root'));
+  ReactDOM.render(
+    <Router>{routes}</Router>,
+    document.getElementById('root'));
 });
